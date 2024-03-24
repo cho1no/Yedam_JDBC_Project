a5d22feddaeb8bd13cadb03f1bfac4e42cf78a00
Administrator <rkawk9735@gmail.com>
2024-03-25 03:52:48 +0900

    240325_sql_update

--- PJ최원호/DB/init_project.sql
+++ PJ최원호/DB/init_project.sql
@@ -1,8 +1,9 @@
-DROP TABLE books;
+
 DROP TABLE book_rental_list;
 DROP TABLE members;
 DROP SEQUENCE book_id_seq;
 DROP SEQUENCE rental_list_seq;
+DROP TABLE books;
 
 -- BOOK TABLE
 CREATE TABLE books
@@ -10,8 +11,10 @@ CREATE TABLE books
     b_no NUMBER(4,0) PRIMARY KEY,
     b_title VARCHAR2(1000 byte) NOT NULL,
     b_writer VARCHAR2(50 char) DEFAULT 'ANY' NOT NULL,
+    b_info VARCHAR2(1000 byte),
     b_inventory NUMBER(2, 0) DEFAULT 0 CHECK(b_inventory > -1),
-    b_created_date DATE DEFAULT sysdate
+    b_created_date DATE DEFAULT sysdate,
+    UNIQUE (b_title, b_writer)
 );
 CREATE SEQUENCE book_id_seq
         START WITH 100
@@ -21,6 +24,7 @@ INSERT INTO books
             b_no
             , b_title
             , b_writer
+            , b_info
             , b_inventory
         )
         VALUES
@@ -28,13 +32,15 @@ INSERT INTO books
             book_id_seq.NEXTVAL
             , '곰돌이 푸'
             , 'A.A. 밀른'
-            , 10
+            , '곰돌이 푸의 이야기'
+            , 9
         );
 INSERT INTO books
         (
             b_no
             , b_title
             , b_writer
+            , b_info
             , b_inventory
         )
         VALUES
@@ -42,6 +48,7 @@ INSERT INTO books
             book_id_seq.NEXTVAL
             , '해리포터와 죽음의 성물'
             , 'J.K. 롤링'
+            , '해리포터 시리즈의 일곱 번째 책이다'
             , 2
         );
 INSERT INTO books
@@ -49,6 +56,7 @@ INSERT INTO books
             b_no
             , b_title
             , b_writer
+            , b_info
             , b_inventory
         )
         VALUES
@@ -56,6 +64,7 @@ INSERT INTO books
             book_id_seq.NEXTVAL
             , '인간실격'
             , '다자이 오사무'
+            , '오직 순수함만을 갈망하던 여린 심성의 한 젊은이가 인간들의 위선과 잔인함에 의해 파멸되어 가는 과정을 그린 책'
             , 3
         );
 
@@ -78,7 +87,7 @@ INSERT INTO members
         (
             'admin'
             , 'admin'
-            , 7
+            , 9
         );
 INSERT INTO members
         (
