f2abf9e444c1993f4d0c578551a2a6eb879f5847
admin <rkawk9735@gmail.com>
2024-03-26 17:46:51 +0900

    240326_sql_update_at_yedam

--- PJ최원호/DB/init_project.sql
+++ PJ최원호/DB/init_project.sql
@@ -1,6 +1,7 @@
-
+DROP TABLE board_notice;
 DROP TABLE book_rental_list;
 DROP TABLE members;
+DROP SEQUENCE board_notice_seq;
 DROP SEQUENCE book_id_seq;
 DROP SEQUENCE rental_list_seq;
 DROP TABLE books;
@@ -67,12 +68,28 @@ INSERT INTO books
             , '오직 순수함만을 갈망하던 여린 심성의 한 젊은이가 인간들의 위선과 잔인함에 의해 파멸되어 가는 과정을 그린 책'
             , 3
         );
-
+INSERT INTO books
+        (
+            b_no
+            , b_title
+            , b_writer
+            , b_info
+            , b_inventory
+        )
+        VALUES
+        (
+            book_id_seq.NEXTVAL
+            , '악의'
+            , '히가시노 게이고'
+            , '일본추리소설'
+            , 0
+        );
 -- MEMBER TABLE
 CREATE TABLE members
 (
     mem_id VARCHAR2(30 byte) PRIMARY KEY
     , mem_pw VARCHAR2(50 byte) NOT NULL
+    , mem_tel VARCHAR2(30 byte) UNIQUE NOT NULL
     , mem_level NUMBER(1, 0) DEFAULT 0
     , mem_join_date DATE DEFAULT sysdate
 );
@@ -81,23 +98,27 @@ INSERT INTO members
         (
             mem_id
             , mem_pw
+            , mem_tel
             , mem_level
         )
         VALUES
         (
             'admin'
             , 'admin'
+            , '010-0000-0000'
             , 9
         );
 INSERT INTO members
         (
             mem_id
             , mem_pw
+            , mem_tel
         )
         VALUES
         (
             'user1'
             , 'user1'
+            , '010-1234-1234'
         );
         
 -- BOOK RENTAL LIST TABLE
@@ -106,6 +127,7 @@ CREATE TABLE book_rental_list
     rent_key NUMBER(3, 0) PRIMARY KEY
     , b_no NUMBER(4,0) NOT NULL
     , renter VARCHAR2(20 byte) NOT NULL
+    , renter_tel VARCHAR2(30 byte) NOT NULL
     , start_rent DATE DEFAULT sysdate
     , end_rent DATE DEFAULT TRUNC(sysdate+14)
     , isReturn NUMBER(1, 0) DEFAULT 0
@@ -119,10 +141,54 @@ INSERT INTO book_rental_list
             rent_key
             , b_no
             , renter
+            , renter_tel
         )
         VALUES
         (
             rental_list_seq.NEXTVAL
             , 101
             , 'admin'
+            , '010-0000-0000'
         );
+INSERT INTO book_rental_list
+        (
+            rent_key
+            , b_no
+            , renter
+            , renter_tel
+        )
+        VALUES
+        (
+            rental_list_seq.NEXTVAL
+            , 102
+            , 'admin'
+            , '010-0000-0000'
+        );
+        
+-- 공지사항
+CREATE TABLE board_notice
+(
+    notice_key NUMBER(3, 0) PRIMARY KEY
+    , notice_title VARCHAR2(100 byte) NOT NULL
+    , notice_detail VARCHAR2(1000 byte) NOT NULL
+    , notice_writer VARCHAR2(30 byte) NOT NULL
+    , notice_write_day DATE DEFAULT sysdate
+    , FOREIGN KEY(notice_writer) REFERENCES members(mem_id)
+);
+CREATE SEQUENCE board_notice_seq
+        NOCYCLE;
+INSERT INTO board_notice
+        (
+            notice_key
+            , notice_title
+            , notice_detail
+            , notice_writer
+        )
+        VALUES
+        (
+            board_notice_seq.NEXTVAL
+            , '도서관리게시판이 열렸습니다.'
+            , '오픈을 축하해주세요!'
+            , 'admin'
+        );
+commit;
